5d68a20df6834d06687485f49bad161e955b907f
Jorge Reis <jorgew.reis@outlook.com>
2023-05-31 15:58:04 -0300

    erros corrigidos

--- content_script.js
+++ content_script.js
@@ -9,25 +9,67 @@ function mostraLocal() {
         let novoLocal = document.querySelector("#barraSuperiorPrincipal > div > div.navbar-collapse > ul > li");
         novoLocal.appendChild(novoElemento);
     }
-}
+};
 
 // Função para consultar se a prioridade é de réu preso
 function consultaPrioridadePreso() {
-    const palavra = 'Prioridade';
-    const trs = [...document.querySelectorAll('#maisDetalhes > dl > dt')];
+    const palavra = 'Réu Preso';
+    const trs = [...document.querySelectorAll('#maisDetalhes > dl > dd')];
     const search = palavra.toLowerCase();
     trs.forEach(el => {
         const matches = el.textContent.toLowerCase().includes(search);
         if (matches) {
             const novaPri = document.createElement("div");
             novaPri.textContent = "Réu Preso";
-            novaPri.id = "titlePrioridades";
+            novaPri.id = "titleReuPreso";
             novoLocal = document.querySelector("form#navbar > ul.navbar-nav");
             novoLocal.appendChild(novaPri);
         }
     });
+};
+
+// crie uma função sleep para aguardar o carregamento da página
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+// Função para incluir assuntos no menu
+async function incluiAssuntos() {
+    let assuntos = document.querySelector("#maisDetalhes > dl > dd:nth-child(4) > ul");
+    let novoAssunto = document.createElement("div");
+    novoAssunto.innerHTML = assuntos.textContent;
+    novoAssunto.id = "titleAssuntos";
+    let liMaisDetalhes = document.querySelector("form#navbar > ul.nav > li.mais-detalhes");
+    liMaisDetalhes.appendChild(novoAssunto);
+  } 
 
-mostraLocal();
-consultaPrioridadePreso();
+// Função para alterar margem superior do conteudo da página
+function alteraMargemSuperior() {
+    const referencia = document.querySelector(".navbar-collapse");
+    const margem = referencia.offsetHeight + 'px';
+    document.querySelector(".timeline").style.top = margem;
+    document.querySelector(".detalhe-documento").style.top = margem;
+};
+
+window.addEventListener('load', function() {
+    sleep(2000);
+    let assuntos = document.querySelector("#maisDetalhes > dl > dd:nth-child(4) > ul");
+    let i = 0;
+    while (!assuntos){
+        sleep(500);
+        assuntos = document.querySelector("#maisDetalhes > dl > dd:nth-child(4) > ul");
+        i++;
+        if (i > 6) {
+            break;
+        }
+    };
+    if (assuntos){
+        incluiAssuntos();
+        consultaPrioridadePreso();
+        alteraMargemSuperior();
+    } else {
+        mostraLocal();
+    }
+    
+});
+
