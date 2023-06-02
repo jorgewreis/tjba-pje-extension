82d94aff3d59d5455e7626f376d27e3f0ecef529
Jorge Reis <jorgew.reis@outlook.com>
2023-06-02 16:39:54 -0300

    upgrade

--- content_script.js
+++ content_script.js
@@ -12,42 +12,176 @@ function mostraLocal() {
 };
 
 function verificaCampos() {
-    let elemento = document.querySelector("#maisDetalhes > dl > dd:nth-child(14)");
-    elemento = elemento.textContent.toString().toUpperCase();
-    let elemento2 = document.querySelector("#maisDetalhes > dl > dd:nth-child(22)");
-    elemento2 = elemento2.textContent.toString().toUpperCase();
-    if ((elemento == "SIM") || (elemento2 != "NÃO")){
-        const novoElemento = document.createElement("div");
+    let par = 0;
+    let imp = 0;
+    let sigiloContent = "";
+    let modeloDigital = "";
+    let Prio = "";
+    
+    for(let i = 1; i <= 30; i++) {
+        for( let j = 1; j <= 15; j++) {
+            if (i % 2 == 0){
+                par = i + 1;
+                imp = i + 2;
+            } else {   
+                par = i;
+                imp = i + 1;
+            }
+        }
+        let linha = document.querySelector("#maisDetalhes > dl > dt:nth-child(" + par + ")").innerHTML;
+        if (linha == "Juízo 100% digital?") {
+            let digital = document.querySelector("#maisDetalhes > dl > dd:nth-child(" + imp + ")");
+            modeloDigital = digital.textContent;
+            break;
+        }              
+    }
+
+    for(let i = 1; i <= 30; i++) {
+        for( let j = 1; j <= 15; j++) {
+            if (i % 2 == 0){
+                par = i + 1;
+                imp = i + 2;
+            } else {   
+                par = i;
+                imp = i + 1;
+            }
+        }
+        let linha = document.querySelector("#maisDetalhes > dl > dt:nth-child(" + par + ")").innerHTML;
+        if (linha == "Segredo de justiça?") {
+            let sigilo = document.querySelector("#maisDetalhes > dl > dd:nth-child(" + imp + ")");
+            sigiloContent = sigilo.textContent;            
+            break;
+        }              
+    }
+
+    for(let i = 1; i <= 30; i++) {
+        for( let j = 1; j <= 15; j++) {
+            if (i % 2 == 0){
+                par = i + 1;
+                imp = i + 2;
+            } else {   
+                par = i;
+                imp = i + 1;
+            }
+        }
+        let linha = document.querySelector("#maisDetalhes > dl > dt:nth-child(" + par + ")").innerHTML;
+        if (linha == "Prioridade?") {
+            let prioridade = document.querySelector("#maisDetalhes > dl > dd:nth-child(" + imp + ")");
+            Prio = prioridade.textContent;            
+            break;
+        }              
+    }
+   
+    if (sigiloContent == "SIM" || Prio != "NÃO" || modeloDigital == "SIM") {
+        let novoElemento = document.createElement("div");
         novoElemento.id = "titleTags";
-        let novoLocal = document.querySelector("form#navbar > ul.navbar-nav");
+        let novoLocal = document.querySelector("#navbar > ul > li");
         novoLocal.appendChild(novoElemento);
+        console.log("Elemento TAG criado");
     }
 }
 
 // Função para mostrar prioridades na barra superior
 function mostraPrioridades() {
-    let elemento = document.querySelector("#maisDetalhes > dl > dd:nth-child(22)");
-    elemento = elemento.textContent.toString().toUpperCase();
-    if (elemento != "NÃO") {
-        const conteudo = elemento;
-        const novoElemento = document.createElement("div");
-        novoElemento.textContent = conteudo;
-        novoElemento.id = "titlePrioridades";
-        let novoLocal = document.querySelector("form#navbar > ul.navbar-nav > div#titleTags");
-        novoLocal.appendChild(novoElemento);
+    let elemento = "";
+  
+    for (let i = 1; i <= 30; i++) {
+      let par, imp;
+  
+      if (i % 2 === 0) {
+        par = i + 1;
+        imp = i + 2;
+      } else {
+        par = i;
+        imp = i + 1;
+      }
+  
+      let linha = document.querySelector("#maisDetalhes > dl > dt:nth-child(" + par + ")").innerHTML;
+      if (linha === "Prioridade?") {
+        let prioridade = document.querySelector("#maisDetalhes > dl > dd:nth-child(" + imp + ")");
+        elemento = prioridade.textContent;
+        break;
+      }
     }
-}
+  
+    // Encontrar elementos após a vírgula
+    let elementosAposVirgula = elemento.split(",").map(elemento => elemento.trim());
+  
+    for (let i = 0; i < elementosAposVirgula.length; i++) {
+      let elementoAtual = elementosAposVirgula[i].toUpperCase();
+      if (elementoAtual !== "NÃO") {
+        let novoElemento = document.createElement("div");
+        novoElemento.textContent = elementoAtual;
+        novoElemento.id = "titlePrioridade";
+        let novoLocal = document.querySelector("#navbar > ul > li > div#titleTags");
+        novoLocal.appendChild(novoElemento);
+      }
+    }
+  }
+  
 
 // Função para mostrar sigilo na barra superior
 function mostraSigilo() {
-    let elemento = document.querySelector("#maisDetalhes > dl > dd:nth-child(14)");
-    elemento = elemento.textContent.toString().toUpperCase();
+    let elemento = "";
+  
+    for (let i = 1; i <= 30; i++) {
+      let par, imp;
+  
+      if (i % 2 === 0) {
+        par = i + 1;
+        imp = i + 2;
+      } else {
+        par = i;
+        imp = i + 1;
+      }
+  
+      let linha = document.querySelector("#maisDetalhes > dl > dt:nth-child(" + par + ")").innerHTML;
+      if (linha === "Segredo de justiça?") {
+        let prioridade = document.querySelector("#maisDetalhes > dl > dd:nth-child(" + imp + ")");
+        elemento = prioridade.textContent;
+        break;
+      }
+    }
+    elemento = elemento.toString().toUpperCase();
     if (elemento == "SIM") {
         const conteudo = "SEGREDO DE JUSTIÇA";
         const novoElemento = document.createElement("div");
         novoElemento.textContent = conteudo;
         novoElemento.id = "titleSigilo";
-        let novoLocal = document.querySelector("form#navbar > ul.navbar-nav > div#titleTags");
+        let novoLocal = document.querySelector("#navbar > ul > li > div#titleTags");
+        novoLocal.appendChild(novoElemento);
+    }
+}
+
+// Função para mostrar Juízo 100% digital na barra superior
+function mostraDigital() {
+    let elemento = "";
+  
+    for (let i = 1; i <= 30; i++) {
+      let par, imp;
+  
+      if (i % 2 === 0) {
+        par = i + 1;
+        imp = i + 2;
+      } else {
+        par = i;
+        imp = i + 1;
+      }
+  
+      let linha = document.querySelector("#maisDetalhes > dl > dt:nth-child(" + par + ")").innerHTML;
+      if (linha === "Juízo 100% digital?") {
+        let prioridade = document.querySelector("#maisDetalhes > dl > dd:nth-child(" + imp + ")");
+        elemento = prioridade.textContent;
+        break;
+      }
+    }
+    elemento = elemento.toString().toUpperCase();
+    if (elemento == "SIM") {
+        const conteudo = "100% DIGITAL";
+        const novoElemento = document.createElement("div");
+        novoElemento.textContent = conteudo;
+        novoElemento.id = "titleDigital";
+        let novoLocal = document.querySelector("#navbar > ul > li > div#titleTags");
         novoLocal.appendChild(novoElemento);
     }
 }
@@ -63,7 +197,7 @@ async function incluiAssuntos() {
     let novoAssunto = document.createElement("div");
     novoAssunto.innerHTML = assuntos.textContent;
     novoAssunto.id = "titleAssuntos";
-    let liMaisDetalhes = document.querySelector("form#navbar > ul.nav > li.mais-detalhes");
+    let liMaisDetalhes = document.querySelector("#navbar > ul > li");
     liMaisDetalhes.appendChild(novoAssunto);
   } 
 
@@ -90,6 +224,7 @@ window.addEventListener('load', function() {
     if (assuntos){
         incluiAssuntos();
         verificaCampos();
+        mostraDigital();
         mostraSigilo();
         mostraPrioridades();
         alteraMargemSuperior();
